675e547ae7f49836e128795c82e48101e9a5bade
Horacio Lerda <41090380+hlerda1@users.noreply.github.com>
2024-02-28 20:24:56 -0300

    002 - Arreglos en modelo Actividades

--- models/actividad.js
+++ models/actividad.js
@@ -3,8 +3,17 @@ const Schema = mongoose.Schema;
 
 const ActividadSchema = Schema({
   descripcion: String,
-  tipo: Number,
+  tipoActividad: String,
+  fechaFin: Date,
+  consigna: String,
   createDtt: { type: Date, default: Date.now },
+  alumnos: [
+    {
+      idAlumno: String, 
+      estado: String, 
+      respuesta: String
+    },
+  ],
 });
 
 module.exports = mongoose.model('Actividad', ActividadSchema);
